7181100d118c515e609c21b8aff3e834b0d396ad
HCS Learning <learning@hardcybersoft.cl>
2020-03-12 02:41:21 -0300

    mejoras con arg

--- for.sh
+++ for.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+base=$1
+
 for anno in $(seq 2017 2020)
 do
 	for mes in $(seq 1 12)
 	do
 		libro=`printf $anno%02d $mes`
-		echo $libro
+		dir="$base/$anno/Compras/DTE/$libro"
+		echo $dir
+		mkdir -p $dir
 	done
 done
 
